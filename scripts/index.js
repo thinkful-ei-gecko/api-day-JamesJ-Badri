753afc51e5ab0a7cf7b386d1f4d09de415fd7b15
Badri-narayan <badritulsiram@gmail.com>
2019-08-23 09:45:31 -0700

    replaced store.addItem

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,11 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
-store.items.push(Item.create('apples'));
